2e25c14e02e9ab1b19fe57147c99ef2288640202
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-04 13:39:52 +0530

    Add files via upload

---
+++ zeronear1.py
@@ -0,0 +1,11 @@
+N=eval(input())
+a=list(map(eval,input().split()))[:N]
+for i in range(0,N-1):
+    for j in range(1,N):
+        if (a[i]+a[j]==0) and (i!=j):
+            print(a[i] , a[j])
+        elif  ((a[i]+a[j]<0.4) and (a[i]+a[j]>0)) and (i!=j):
+            print(a[i] , a[j])
+        elif ((a[i]+a[j]>-0.4) and (a[i]+a[j]<0)) and (i!=j):
+            print(a[i] , a[j])
+        
