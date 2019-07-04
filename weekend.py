7827298701e102b60f14c524301fbf1e4e93bdac
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-04 17:12:28 +0530

    Add files via upload

---
+++ weekend.py
@@ -0,0 +1,11 @@
+a=str(input())
+b=str(input())
+hold=["SUNDAY","SATURDAY"]
+if a.upper() in hold:
+    print("yes")
+else:
+    print("no")
+if b.upper() in hold:
+    print("yes")
+else:
+    print("no")
