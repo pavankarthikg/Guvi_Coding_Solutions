6a164b0e3d13e4d5dca9e809852d45414e04832d
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-04 11:14:32 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -1,15 +1,17 @@
 N=int(input())
-g = list(map(int,input().split()))[:N]
 s=[]
+g = list(map(int,input().split()))[:N]
 for i in range(0,N):
     for j in range((i+1),N):
         if(g[i]==g[j]):
             s.append(g[i])
 s.sort()
-if(len(s)!=0):
-    print(*s,sep=" ")
-else:
+if(len(s)==0):
     print("unique")
+else:
+    print(*s,sep=" ")
+ 
+    
 
 
             
