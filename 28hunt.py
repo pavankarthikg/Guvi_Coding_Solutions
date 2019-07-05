8c283d934753f9ffb564861f8e41bdfd3cb4101a
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-05 12:22:16 +0530

    Add files via upload

--- 28hunt.py
+++ 28hunt.py
@@ -1,7 +1,12 @@
 s=str(input())
 l=len(s)
+count=0
 for i in range (0,l-1):
     for j in range (i+1,l):
         if(s[i]==s[j]):
-            f.append(a[i])
+            count+=1
+        else:
+            count=0
+if (count>=0):
+    print(s[i])
     
