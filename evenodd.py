f4081fde96334e8b042938280bcf8df8faf0f3af
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-05 10:04:14 +0530

    Add files via upload

--- evenodd.py
+++ evenodd.py
@@ -1,11 +1,9 @@
 N=int(input())
 a=list(map(int, input().split()))[:N]
 for i in range (0,N):
-    if (i%2)!=0:
+    if ((i%2)!=0):
         if (a[i]%2)==0:
             print(a[i], end=' ')
-        else:
-            break
-    else:
+    elif ((i%2)==0):
         if (a[i]%2)!=0:
             print(a[i], end=' ')
