9ec649c53723e1a21479bb940ba07b1e45e41b31
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-10 22:44:00 +0530

    Update updpro5.py

--- updpro5.py
+++ updpro5.py
@@ -1,13 +1,6 @@
 n,a,b=map(int,input().split())
 ct=0
-for i in range (0,n):
-    if n>2:
-        if ((a+b)*i)==n//2:
-            ct+=1  
-    if (n==2):
-        if ((a+b)*i)==2:
-            ct+=1             
-if (ct==1):
+if (n%(a+b)==0):
     print("YES")
-else:
+ else:
     print("NO")
