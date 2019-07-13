0742f89162ea8c272a149417b9ee54eb075035c3
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-13 16:45:38 +0530

    Update pro53.py

--- pro53.py
+++ pro53.py
@@ -2,7 +2,7 @@ import string
 s=str(input())
 alphabet = set(string.ascii_lowercase)   
 def ispangram(string): 
-    return set(string.lower()) >= alphabet  
+    return set(string.lower())>= alphabet  
 if(ispangram(s) == True): 
     print("yes") 
 else: 
