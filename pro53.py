d10fa44c41e64d6572975c8d39c50b13601744aa
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-13 16:43:14 +0530

    Add files via upload

---
+++ pro53.py
@@ -0,0 +1,9 @@
+import string
+s=str(input())
+alphabet = set(string.ascii_lowercase)   
+def ispangram(string): 
+    return set(string.lower()) >= alphabet  
+if(ispangram(s) == True): 
+    print("yes") 
+else: 
+    print("no") 
