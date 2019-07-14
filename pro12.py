0c4b0403e9b96a7d7ad14ea0453004714f2fcfd0
pavankarthikg <49197349+pavankarthikg@users.noreply.github.com>
2019-07-14 22:55:24 +0530

    Add files via upload

---
+++ pro12.py
@@ -0,0 +1,8 @@
+n,q=map(int,input().split())
+li=list(map(int,input().split()))[:n]
+l=[]
+for _ in range (0,q):
+    u,v=map(int,input().split())
+    l.append(sum(li[u-1:v]))
+for i in l:
+    print(i)
